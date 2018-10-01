9027dae7127f60036d626b339c59915872c26104
Ruxuan Li <ruxuan.li@gmail.com>
2018-10-01 19:04:17 -0400

    Added basic db setup steps for POC

---
+++ POC/constants.py
@@ -0,0 +1,16 @@
+ALGOMA       = "Algoma"
+BRANT        = "Brant"
+CHATHAM_KENT = "Chatham/Kent"
+
+
+CENSUS_DIVISION_TO_ID = {
+        ALGOMA: 0,
+        BRANT: 1,
+        CHATHAM_KENT: 2
+}
+
+ID_TO_CENSUS_DIVISION = {
+        0: ALGOMA,
+        1: BRANT,
+        2: CHATHAM_KENT,
+}
