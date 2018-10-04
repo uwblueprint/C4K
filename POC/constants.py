3550a7e4caf3565501bdac01513f2d5054d68c56
Ruxuan Li <ruxuan.li@gmail.com>
2018-10-04 13:01:50 -0400

    Updated setup db to add subset of demographics information

--- POC/constants.py
+++ POC/constants.py
@@ -2,7 +2,6 @@ ALGOMA       = "Algoma"
 BRANT        = "Brant"
 CHATHAM_KENT = "Chatham/Kent"
 
-
 CENSUS_DIVISION_TO_ID = {
         ALGOMA: 0,
         BRANT: 1,
@@ -13,4 +12,10 @@ ID_TO_CENSUS_DIVISION = {
         0: ALGOMA,
         1: BRANT,
         2: CHATHAM_KENT,
+}
+
+CENSUS_FILE_PATH = {
+        ALGOMA: "algoma/algoma_census_2016.csv",
+        BRANT: "brant/brant_census_2016.csv",
+        CHATHAM_KENT: "chatham_kent/chatham_kent_census_2016.csv"
 }
