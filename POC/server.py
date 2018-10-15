87e837df807e4f180e579f4ad1b123274d376c88
Atty <AttyXY@gmail.com>
2018-10-15 18:24:33 -0400

    Added get_data_by_id to server.py

--- POC/server.py
+++ POC/server.py
@@ -5,7 +5,7 @@ import db
 app = Flask(__name__)
 
 @app.route("/<census_division>")
-def get_data(census_division):
+def get_data_by_division(census_division):
     # Check if census_divison is valid
     if census_division not in constants.CENSUS_DIVISION_TO_ID:
         return jsonify({ "error": "Invalid census division" })
@@ -13,6 +13,15 @@ def get_data(census_division):
     census_division_id = constants.CENSUS_DIVISION_TO_ID[census_division]
     data = db.get_census_division_data(census_division_id)
     return jsonify({ "error": "", "data": data })
+
+@app.route("/id/<int:census_id>")
+def get_data_by_id(census_id):
+    # Check if census_id is valid
+    if census_id not in constants.ID_TO_CENSUS_DIVISION:
+        return jsonify({ "error": "Invalid census id" })
+
+    data = db.get_census_division_data(census_id)
+    return jsonify({ "error": "", "data": data }) 
 
 if __name__ == "__main__":
     app.run("localhost", 8080, debug=True)
