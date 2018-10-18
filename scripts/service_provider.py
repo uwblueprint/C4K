68725d6cc6cd7d124a1e3ebde55e977fba3c1a28
Jerry Xie <jerry_xie@hotmail.com>
2018-10-18 19:43:49 -0400

    Cleaned up service provider script.

--- scripts/service_provider.py
+++ scripts/service_provider.py
@@ -1,14 +1,21 @@
 import csv
 
+# Script for parsing raw CYMH data into unique service providers
+
+SERVICE_PROVIDER_INITIAL = '../data/service_providers/service_providers_raw.csv'
+SERVICE_PROVIDER_PARSED = '../data/service_providers/service_providers_parsed.csv'
+
 websites = {}
 
-# 12, 13, 16
-with open('../data/service_providers/service_providers_raw.csv', newline='', encoding="ISO-8859-1") as file:
+with open(SERVICE_PROVIDER_INITIAL, newline='', encoding="ISO-8859-1") as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
+        # Reading name from coloumn 1, and website from column 16
         site = row[15].split('/')
         name = row[0].split(' - ')[0]
-        # Use domain of website as key, if no website, use name
+
+        # Use domain of website as key, if no website, use provider name as key
+        # eg: { 'www.kidshealth.com' : 'Kids Health', 'website-less' : 'website-less' }
         if len(site) > 2:
             key = site[2]
         else:
@@ -18,18 +25,17 @@ with open('../data/service_providers/service_providers_raw.csv', newline='', enc
             websites[key] = name
 
         
-with open('../data/service_providers/service_providers_parsed.csv', 'w', newline='', encoding="ISO-8859-1") as file:
+with open(SERVICE_PROVIDER_PARSED, 'w', newline='', encoding="ISO-8859-1") as file:
     writer = csv.writer(file, delimiter=',')
     # Write headers
     writer.writerow(['Name', 'Website'])
     for key in websites:
+
         # No website found
         if key == websites[key]:
             writer.writerow([key])
+        # Insert name and website info
         else:
             writer.writerow([websites[key], key])
 
-# for key in websites:
-#     print('Name: {}, Website: {}'.format(websites[key], key))
-
-print(len(websites))
+print(('{} providers parsed.').format(len(websites)))
