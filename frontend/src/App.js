9eb37f807339d91e9995e30cbd5972173e571803
James Lu <31876783+JamesZhaoLu@users.noreply.github.com>
2018-11-08 18:48:52 -0500

    Add material ui and nav (#20)
    * added basic map view
    
    * added basic fixes to App.js
    
    * added basics of NavBar and Material UI
    
    * added Sidebar.css and basic stylinng
    
    * removed arcgis dependency
    
    * changed to single Component formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Map from './components/Map';
+import Sidebar from './components/Sidebar';
 
 class App extends Component {
-  
   render() {
     return (
       <div>
-        <Map></Map>
+        <Sidebar />
+        <Map />
       </div>
     );
   }
