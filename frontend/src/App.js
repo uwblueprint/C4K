e95ca566e3539e938648483cba8f85613a88ed87
James Lu <james.zhao.lu@gmail.com>
2018-11-08 18:47:27 -0500

    changed to single Component formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Sidebar></Sidebar>
-        <Map></Map>
+        <Sidebar />
+        <Map />
       </div>
     );
   }
