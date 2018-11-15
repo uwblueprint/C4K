da76ea9ea509a5796371b7563818a8dfb6a181c0
James Lu <31876783+JamesZhaoLu@users.noreply.github.com>
2018-11-15 18:14:20 -0500

    added appReducer; created toggle feature, refactored component code (#22)
    * added appReducer; created toggle feature, refactored component code
    
    * added color to denote selected toggle
    
    * added unselected color
    
    * removed loggin; renamed files for readability

---
+++ frontend/src/components/ListView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './ListView.css';
+
+class ListView extends Component {
+	render() {
+		return (
+			<div className="centerDiv">
+				<h1 className="titleText">This is the List View</h1>
+			</div>
+		);
+	}
+}
+
+export default ListView;
