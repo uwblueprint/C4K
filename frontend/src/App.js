4c6987f22260d14ea272d6e932597264dbcb2b47
James Lu <31876783+JamesZhaoLu@users.noreply.github.com>
2018-11-15 18:14:20 -0500

    added appReducer; created toggle feature, refactored component code (#22)
    * added appReducer; created toggle feature, refactored component code
    
    * added color to denote selected toggle
    
    * added unselected color
    
    * removed loggin; renamed files for readability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
 import './App.css';
+
+// Redux
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {changeView} from './actions/changeViewActions';
+
+// constants 
+import * as constants from './constants/viewConstants';
+
+// components
 import Map from './components/Map';
 import Sidebar from './components/Sidebar';
 import ToggleView from './components/ToggleView';
+import ListView from './components/ListView';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Sidebar />
-        <Map />
-        <ToggleView />
+        {this.props.view === constants.MAP_VIEW ? <Map /> : <ListView /> }
+        <ToggleView view={this.props.view} changeView={this.props.changeView}/>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    view: state.changeViewReducer.view
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    changeView: bindActionCreators(changeView, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
