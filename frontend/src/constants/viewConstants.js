be5d88c708587cb4882a4c40c34e3d40f9b4486a
James Lu <31876783+JamesZhaoLu@users.noreply.github.com>
2018-11-15 18:14:20 -0500

    added appReducer; created toggle feature, refactored component code (#22)
    * added appReducer; created toggle feature, refactored component code
    
    * added color to denote selected toggle
    
    * added unselected color
    
    * removed loggin; renamed files for readability

---
+++ frontend/src/constants/viewConstants.js
@@ -0,0 +1,2 @@
+export const MAP_VIEW = "MAP_VIEW";
+export const LIST_VIEW = "LIST_VIEW";
---
+++ frontend/src/reducers/initialState.js
@@ -0,0 +1,5 @@
+import * as constants from '../constants/viewConstants';
+
+export default {
+  view: constants.MAP_VIEW
+};
