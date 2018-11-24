a77079d9742113c2e1be929eaa28272ca22313a3
James Lu <james.zhao.lu@gmail.com>
2018-11-24 02:43:04 -0500

    added mutiple ListViewCards

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -8,7 +8,23 @@ class ListView extends Component {
 	render() {
 		return (
 			<div className="listView">
-				<ListViewCard 
+				<ListViewCard
+					title={"Lutherwood - Safe Haven"}
+					subTitle={"Lead Agency"}
+					location={"Waterloo"}
+					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
+					phone={"(519) 749 - 1450"}
+					site={"https://www.lutherwood.ca/"}
+				/>
+				<ListViewCard
+					title={"Lutherwood - Safe Haven"}
+					subTitle={"Lead Agency"}
+					location={"Waterloo"}
+					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
+					phone={"(519) 749 - 1450"}
+					site={"https://www.lutherwood.ca/"}
+				/>
+				<ListViewCard
 					title={"Lutherwood - Safe Haven"}
 					subTitle={"Lead Agency"}
 					location={"Waterloo"}
