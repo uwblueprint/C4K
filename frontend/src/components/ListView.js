7a6b821c9c0d205385c9602f3d8460d8d6eafd34
James Lu <james.zhao.lu@gmail.com>
2018-11-26 18:25:40 -0500

    added code for ListViewCards and styling

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -9,32 +9,32 @@ class ListView extends Component {
 		return (
 			<div className="listView">
 				<ListViewCard
-					title={"Lutherwood - Safe Haven"}
-					subTitle={"Lead Agency"}
+					name={"Lutherwood - Safe Haven"}
+					type={"Lead Agency"}
 					location={"Waterloo"}
 					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
 					phone={"(519) 749 - 1450"}
 					site={"https://www.lutherwood.ca/"}
 				/>
 				<ListViewCard
-					title={"Lutherwood - Safe Haven"}
-					subTitle={"Lead Agency"}
+					name={"Lutherwood - Safe Haven"}
+					type={"Lead Agency"}
 					location={"Waterloo"}
 					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
 					phone={"(519) 749 - 1450"}
 					site={"https://www.lutherwood.ca/"}
 				/>
 				<ListViewCard
-					title={"Lutherwood - Safe Haven"}
-					subTitle={"Lead Agency"}
+					name={"Lutherwood - Safe Haven"}
+					type={"Lead Agency"}
 					location={"Waterloo"}
 					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
 					phone={"(519) 749 - 1450"}
 					site={"https://www.lutherwood.ca/"}
 				/>
 				<ListViewCard
-					title={"Lutherwood - Safe Haven"}
-					subTitle={"Lead Agency"}
+					name={"Lutherwood - Safe Haven"}
+					type={"Lead Agency"}
 					location={"Waterloo"}
 					address={"41 Weber St W, Kitchener, ON N2H 4Z1"}
 					phone={"(519) 749 - 1450"}
