8915869828f83fa9432098ee8750aad891286c3f
silastsui <silastsuii@gmail.com>
2018-11-26 19:53:34 -0500

    Add auth for new notes to service providers

--- backend/server.py
+++ backend/server.py
@@ -46,6 +46,9 @@ def get_all_service_providers():
 
 @app.route("/service_providers/<int:service_provider_id>/notes")
 def update_service_provider_notes(service_provider_id):
+    if not is_admin(request.args.get('id_token'))
+        return jsonify({"error": "User is not an admin"})
+    
     notes = request.args.get('notes')
     if notes is None:
         return jsonify({"error": "Expecting an argument for notes"})
@@ -78,6 +81,8 @@ def create_user():
 
     return jsonify({'uid': user.uid})
 
+def is_admin(id_token):
+    return auth.verify_id_token(id_token)['admin']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Server arguments')
