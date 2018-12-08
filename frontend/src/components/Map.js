14def05157ccb730e0883aca88b02eaa4e5fe286
James Lu <james.zhao.lu@gmail.com>
2018-12-08 15:12:21 -0500

    added onFeature function to each census division

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -3,13 +3,44 @@ import './Map.css';
 import * as L from 'leaflet';
 import * as esri from 'esri-leaflet';
 
+
+// This is the default style of layers when the user isn't interacting
+// with the map
+let quiet = {
+  fillColor: '#3399FF',
+  fillOpacity: 0.2,
+  color: 'white',
+  opacity: 0.6,
+  weight: 1,
+  dashArray: 0
+};
+
+// The style of a layer that's been filtered to but not the specific
+// shape the user selected
+let medium = {
+  fillColor: '#D6EBFF',
+  fillOpacity: 0.4,
+  opacity: 1,
+  weight: 1
+};
+
+// The specific shape selected
+let loud = {
+  fillColor: '#FFFF00',
+  fillOpacity: 0.8,
+  opacity: 0.2,
+  weight: 3,
+  dashArray: '3'
+};
+
 class Map extends Component {
+
   constructor(props) {
     super(props);
 
     //the map is handled by the state: e.g. this.state.map.zoomIn() to zoom
     this.state = {currentZoomLevel: 7, map: null, tileLayer: null};
-    this.onMouseClick = this.onMouseClick.bind(this);
+    //this.onEachFeature = this.onEachFeature.bind(this);
   }
 
   componentDidMount() {
@@ -20,28 +51,27 @@ class Map extends Component {
     document.getElementById('map').title = event.layer.feature.properties.CDNAME;
   }
 
-  onMouseClick(event) {
-    
-    // Get the values of a feature
-    let layer_id = event.layer.feature.id;
-    let feature = event.layer.feature;
-    let properties = feature.properties;
-    console.log(event.layer._leaflet_id);
-    console.log(event.layer.feature.id);
-    console.log(this);
-    console.log(this.props);
-    // this is not bound to props here
-    console.log(this.state.map)
-    console.log(this.props.selectedCensusDivision);
+  onEachFeature(feature, layer) {
+    console.log(layer);
+    console.log(this.state.map._layers);
+    // console.log(feature);
+    // console.log(this.props);
+    layer.on('click', (e) => { 
+      layer.setStyle({fillColor: '#FFFF00'});
 
-    // change selected Census Division
-    this.props.selectCensusDivision(properties.CDNAME);
+      // Check if layer is already selected
+      let cdID = parseInt(feature.properties.CDUID, 10);
+      if (cdID !== this.props.selectedCensusDivision) {
+        // fire an action to change the new census division
+        this.props.selectCensusDivision(cdID);
+        // this.state.map.onEachLayers
+      }
+      // else do nothing - they already have the census division selected
 
-    // Highlight the Census Division
-    // this.state.map._layers[layer_id].setStyle(
-    // {
-    //   'weight': 3,
-    // })
+      // update the style of all the census divisions
+
+
+    });
   }
 
   //Loading the Map, this only gets called once.
@@ -65,10 +95,10 @@ class Map extends Component {
         var opacity = Math.max(Math.min(1, Math.log(density)/5), 0.05); // not used for now
         // in the province of Ontario
         return { weight: 1, opacity, fillOpacity: 0.3, color: 'black', fillColor: '#2526A9' };
-      }
+      },
+      onEachFeature: this.onEachFeature.bind(this),
     })
     .on('mouseover', this.onMouseHandler)
-    .on('click', this.onMouseClick)
     .addTo(map);
 
     this.setState({map, tileLayer});
